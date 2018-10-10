68a0396c6f66135802df0dc4d07d0d6faa49fbc3
affixusimac1 <viveking@outlook.com>
2018-10-10 14:34:39 +0530

    d

--- Floooaatt/ViewController.swift
+++ Floooaatt/ViewController.swift
@@ -7,12 +7,47 @@
 //
 
 import UIKit
+import Floaty
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let FAB = Floaty()
+        FAB.buttonColor = UIColor.green
+        FAB.plusColor = UIColor.white
+//        FAB.addItem("Public Beep", icon: UIImage(named: "ic"))
+//        FAB.addItem("Notice", icon: UIImage(named: "ic"))
+//        FAB.addItem("Circular", icon: UIImage(named: "ic"))
+//        FAB.addItem("Notification", icon: UIImage(named: "ic"))
+//        FAB.addItem("Others", icon: UIImage(named: "ic"))
+        
+        let folderFloaty = FAB.addItem("Folder", icon: UIImage(named: "ic"), titlePosition:.left, handler: { (item) in
+            print("Firstt")
+            FAB.close()
+        })
+        folderFloaty.buttonColor = UIColor.green
+        folderFloaty.iconTintColor = .white
+        let folderFloaty1 = FAB.addItem("Folder", icon: UIImage(named: "ic"), titlePosition:.left, handler: { (item) in
+            print("Firstt")
+            FAB.close()
+        })
+        folderFloaty1.buttonColor = UIColor.green
+        folderFloaty1.iconTintColor = .white
+        let folderFloaty2 = FAB.addItem("Folder", icon: UIImage(named: "ic"), titlePosition:.left, handler: { (item) in
+            print("Firstt")
+            FAB.close()
+        })
+        folderFloaty2.buttonColor = UIColor.green
+        folderFloaty2.iconTintColor = .white
+        let folderFloaty3 = FAB.addItem("Folder", icon: UIImage(named: "ic"), titlePosition:.left, handler: { (item) in
+            print("Firstt")
+            FAB.close()
+        })
+        folderFloaty3.buttonColor = UIColor.green
+        folderFloaty3.iconTintColor = .white
+         self.view.addSubview(FAB)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +55,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func one(){
+        
+    }
 }
+
 
